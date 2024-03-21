e74e8c1304b613dfb3dc9baaafd2f1975da20483
richardhestencz <richard.hestencz@gmail.com>
2024-03-21 16:43:21 +0100

    snakeorai

--- snake/Form1.cs
+++ snake/Form1.cs
@@ -2,9 +2,97 @@ namespace snake
 {
     public partial class Form1 : Form
     {
+        int fejX;
+        int fejY;
+        int irány_x;
+        int irány_y;
+        int LépésSzám;
+        List<KigyóElem> kígyó = new List<KigyóElem>();
         public Form1()
         {
             InitializeComponent();
+
+            fejX = 100;
+            fejY = 100;
+            irány_x = 1;
+            irány_y = 0;
+            LépésSzám = 0;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            LépésSzám++;
+           
+            //Fejnövesztés
+            fejX += irány_x * KigyóElem.Méret;
+            fejY += irány_y * KigyóElem.Méret;
+
+            foreach (object item in Controls)
+            {
+                if (item is KigyóElem)
+                {
+                    KigyóElem k = (KigyóElem)item;
+
+                    if (k.Top == fejY && k.Left == fejX)
+                    {
+                        timer1.Enabled = false;
+                        return;
+                    }
+                }
+            }
+
+            if (Controls.Count > KigyóElem.hossz)
+                {
+                    KigyóElem levágandó = kígyó[0];
+                    kígyó.RemoveAt(0);
+                    Controls.Remove(levágandó);
+                }
+
+            
+
+            KigyóElem ke = new KigyóElem();
+            ke.Top = fejY;
+            ke.Left = fejX;
+            kígyó.Add(ke);
+            Controls.Add(ke);
+            if (LépésSzám % 2 == 0) ke.BackColor = Color.Yellow;
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                irány_y = -1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                irány_y = 1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                irány_y = 0;
+                irány_x = -1;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                irány_y = 0;
+                irány_x = 1;
+            }
         }
     }
 }
---
+++ snake/KigyóElem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    internal class KigyóElem : PictureBox
+    {
+        public static int Méret = 20;
+        public static int hossz = 15;
+
+        public KigyóElem()
+        {
+            Width = KigyóElem.Méret;
+            Height = KigyóElem.Méret;
+            BackColor = Color.Fuchsia;
+        }
+    }
+}
